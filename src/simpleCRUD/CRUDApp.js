6922eddc87d6888aeba71104a7266ee9c0059bc5
Meghamala KM <meghamala.km@logituit.com>
2023-10-03 17:21:05 +0530

    crud

---
+++ src/simpleCRUD/CRUDApp.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CRUDApp = () => {
+  return (
+    <View>
+      <Text>CRUDApp</Text>
+    </View>
+  )
+}
+
+export default CRUDApp
